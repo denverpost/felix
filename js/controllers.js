d4afad3a5965d1399bb5cac73b9438f3f9d48b73
freejoe76 <joe.murphy@gmail.com>
2014-09-15 10:00:54 -0400

    Start on project URL routing

---
+++ js/controllers.js
@@ -0,0 +1,7 @@
+var felixControllers = angular.module('felixControllers', []);
+
+
+felixControllers.controller('ProjectIndexCtrl', ['$scope', '$http',
+    function($scope, $http)
+    {
+    }]);
---
+++ js/felix.js
@@ -0,0 +1,25 @@
+var app = angular.module("felix", [
+    'ngRoute',
+    'felixControllers']);
+
+app.controller('getData', function($scope, $http) {
+    $http.get('data/test.json').
+        success(function(data, status, headers, config) {
+            $scope.test = data;
+        }).
+        error(function(data, status, headers, config) {
+            console.log('an error');
+        });
+});
+
+// URL routing
+app.config(['$routeProvider',
+    function ($routeProvider)
+    {
+        $routeProvider.
+            when('/projects',
+            {
+                templateURL: 'partials/projects-index.html',
+                controller: 'ProjectIndexCtrl'
+            });
+    }]);
