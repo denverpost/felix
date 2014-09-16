0293523e7e3c1c15979a8ab034c64a94d279a323
freejoe76 <joe.murphy@gmail.com>
2014-09-16 09:00:53 -0400

    Make the submit button do something. Also, start work on transmitting the form meta information.

--- js/controllers.js
+++ js/controllers.js
@@ -10,5 +10,43 @@ felixControllers.controller('ProjectIndexCtrl', ['$scope', '$http',
 felixControllers.controller('ProjectAddCtrl', ['$scope', '$http',
     function($scope, $http)
     {
+        $scope.submit = function(form)
+        {
+            $scope.submitted = true;
 
+            if ( form.$invalid ) return;
+            console.log(form, $scope);
+
+            var submission = {
+                params: {
+                    'object': $scope.object,
+                    'action': $scope.action,
+                    'name': $scope.name,
+                    'slug': $scope.slug
+                }
+            }
+
+            // It's valid, so let's send the data to our backend form handler
+            // and write it to a file.
+
+            // ^^^ NOTE: Domain of handler is hard-coded, needs to not be.
+            $http.jsonp('http://localhost/felix/handler.php', submission)
+                .success(function(data, status, headers, submission)
+                {
+                    console.log(data, status, headers);
+                    if ( data.status == 'OK' )
+                    {
+                    }
+                    else
+                    {
+                        // error handlers
+                    }
+                })
+                .error(function(data, status, headers, submission)
+                {
+                    console.log(data, status, headers);
+
+                });
+
+        }
     }]);
