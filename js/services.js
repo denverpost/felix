dd31a846cf36c8498ba2aff4ab932e950e13f258
freejoe76 <joe.murphy@gmail.com>
2014-09-22 14:47:40 -0600

    How do we handle data? Right now it's with .

--- js/services.js
+++ js/services.js
@@ -2,8 +2,8 @@ var felixServices = angular.module('felixServices', ['ngResource']);
 
 felixServices.factory('dataFactory', ['$resource',
 	function($resource){
-		return $resource('data/:jsonId.json', {}, {
-			query: {method:'GET', params:{jsonId:'test'}, isArray:false}
+		return $resource('data/projects/:jsonSlug/project.json', {}, {
+			query: {method:'GET', params:{jsonSlug:'joe-test'}, isArray:false}
 			/* getJSON: function(callback) {
 				$http.get('data/test.json').success(callback);
 			} */
